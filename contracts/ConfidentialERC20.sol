377d78355cc34aff3013da7e8f2bc76b748d36e8
Mihir Sahu <114948140+0xmihirsahu@users.noreply.github.com>
2025-05-17 03:38:14 +0530

    Fixed ConfidentialERC20.sol

--- contracts/ConfidentialERC20.sol
+++ contracts/ConfidentialERC20.sol
@@ -1,33 +1,35 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.24;
 
 import "@inco/lightning/src/Lib.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 contract ConfidentialERC20 is Ownable2Step {
-    // Events
+    
+    // Events for Transfer, Approval, Mint, and Decryption
     event Transfer(address indexed from, address indexed to);
     event Approval(address indexed owner, address indexed spender);
     event Mint(address indexed to, uint256 amount);
     event UserBalanceDecrypted(address indexed user, uint256 decryptedAmount);
 
-    // State variables
     uint256 public _totalSupply;
     string public _name;
     string public _symbol;
     uint8 public constant decimals = 18;
 
-    // Encrypted state
+    // Mappings for balances and allowances
     mapping(address => euint256) internal balances;
     mapping(address => mapping(address => euint256)) internal allowances;
     mapping(uint256 => address) internal requestIdToUserAddress;
 
+    // Constructor to set the token name, symbol, and owner
     constructor(string memory name, string memory symbol) Ownable(msg.sender) {
         _name = name;
         _symbol = symbol;
     }
 
-    // Minting functions
+    // Mint function to create tokens and add to the owner's balance
     function mint(uint256 mintedAmount) public virtual onlyOwner {
         balances[owner()] = e.add(
             balances[owner()],
@@ -39,7 +41,10 @@ contract ConfidentialERC20 is Ownable2Step {
         emit Mint(owner(), mintedAmount);
     }
 
-    function _mint(bytes calldata encryptedAmount) public virtual {
+    // Overloaded _mint function to allow encrypted token minting
+    function _mint(
+        bytes calldata encryptedAmount
+    ) public virtual /*onlyOwner*/ {
         balances[msg.sender] = e.add(
             balances[msg.sender],
             e.newEuint256(encryptedAmount, msg.sender)
@@ -49,7 +54,7 @@ contract ConfidentialERC20 is Ownable2Step {
         e.allow(balances[msg.sender], msg.sender);
     }
 
-    // Transfer functions
+    // Transfer function for EOAs using encrypted inputs
     function transfer(
         address to,
         bytes calldata encryptedAmount
@@ -58,6 +63,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return true;
     }
 
+    // Transfer function for contracts
     function transfer(
         address to,
         euint256 amount
@@ -67,12 +73,12 @@ contract ConfidentialERC20 is Ownable2Step {
         return true;
     }
 
-    // View functions
+    // Retrieves the balance handle of a specified wallet
     function balanceOf(address wallet) public view virtual returns (euint256) {
         return balances[wallet];
     }
 
-    // Approval functions
+    // Approve function for EOAs with encrypted inputs
     function approve(
         address spender,
         bytes calldata encryptedAmount
@@ -81,6 +87,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return true;
     }
 
+    // Approve function for contracts
     function approve(
         address spender,
         euint256 amount
@@ -90,6 +97,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return true;
     }
 
+    // Internal function to handle allowance approvals
     function _approve(
         address owner,
         address spender,
@@ -101,14 +109,15 @@ contract ConfidentialERC20 is Ownable2Step {
         e.allow(amount, spender);
     }
 
-    // Allowance functions
+    // Retrieves the allowance handle for a spender
     function allowance(
         address owner,
         address spender
     ) public view virtual returns (euint256) {
         return _allowance(owner, spender);
     }
-
+    
+    // Internal function to retrieve an allowance handle
     function _allowance(
         address owner,
         address spender
@@ -116,7 +125,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return allowances[owner][spender];
     }
 
-    // TransferFrom functions
+    // TransferFrom function for EOAs with encrypted inputs
     function transferFrom(
         address from,
         address to,
@@ -126,6 +135,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return true;
     }
 
+    // TransferFrom function for contracts
     function transferFrom(
         address from,
         address to,
@@ -136,7 +146,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return true;
     }
 
-    // Internal helper functions
+    // Internal function to update an allowance securely
     function _updateAllowance(
         address owner,
         address spender,
@@ -162,6 +172,7 @@ contract ConfidentialERC20 is Ownable2Step {
         return isTransferable;
     }
 
+    // Internal transfer function for encrypted token transfer
     function _transfer(
         address from,
         address to,
@@ -184,5 +195,32 @@ contract ConfidentialERC20 is Ownable2Step {
         e.allow(newBalanceFrom, from);
 
         emit Transfer(from, to);
+    }
+
+    // Owner-only function to request decryption of a user's balance
+    function requestUserBalanceDecryption(
+        address user
+    ) public onlyOwner returns (uint256) {
+        euint256 encryptedBalance = balances[user];
+        e.allow(encryptedBalance, address(this));
+
+        uint256 requestId = e.requestDecryption(
+            encryptedBalance,
+            this.onDecryptionCallback.selector,
+            ""
+        );
+        requestIdToUserAddress[requestId] = user;
+        return requestId;
+    }
+
+    // Callback function to handle decrypted balance for a user
+    function onDecryptionCallback(
+        uint256 requestId,
+        bytes32 _decryptedAmount,
+        bytes memory data
+    ) public returns (bool) {
+        address userAddress = requestIdToUserAddress[requestId];
+        emit UserBalanceDecrypted(userAddress, uint256(_decryptedAmount));
+        return true;
     }
 }
